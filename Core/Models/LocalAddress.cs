8f447e165fbb974f3df4b1f46654eb3d0c7a9f69
NTPhatFPT <phatntse150133@fpt.edu.vn>
2023-09-21 13:59:26 +0700

    update Model

--- Core/Models/LocalAddress.cs
+++ Core/Models/LocalAddress.cs
@@ -13,8 +13,6 @@ namespace Core.Models
 
         public int LocalId { get; set; }
         public string WardName { get; set; } = null!;
-        public string DistrictName { get; set; } = null!;
-        public string CityName { get; set; } = null!;
 
         public virtual ICollection<StoreDesciption> StoreDesciptions { get; set; }
         public virtual ICollection<User> Users { get; set; }
