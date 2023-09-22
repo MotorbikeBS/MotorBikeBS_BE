7fce19feca359b0885e46758a29f37b2fc6cbcb7
NTPhatFPT <phatntse150133@fpt.edu.vn>
2023-09-22 18:52:15 +0700

    update ApiResponse

--- API/DTO/UserDTO/RegisterDTO.cs
+++ API/DTO/UserDTO/RegisterDTO.cs
@@ -5,9 +5,11 @@ namespace API.DTO.UserDTO
 {
     public class RegisterDTO
     {
-        [Required]
-        [SingleSpaceBetweenNamesAttribute(ErrorMessage = "Not Valid!")]
-        public string? UserName { get; set; }
+
+		[Required(AllowEmptyStrings = false, ErrorMessage = "Please enter the name")]
+		[StringLength(maximumLength: 25, MinimumLength = 10, ErrorMessage = "Length must be between 10 to 25")]
+
+		public string? UserName { get; set; }
         [Required, EmailAddress]
         public string Email { get; set; } = null!;
         [Required, MinLength(6)]
