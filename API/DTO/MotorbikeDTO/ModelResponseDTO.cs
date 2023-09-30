4baef8deb957722b31ca9998b81548e66afa6a79
Trần Quang Hùng <97095862+AvilXx@users.noreply.github.com>
2023-09-30 22:21:20 +0700

    Revert "Revert "Hung tq""

---
+++ API/DTO/MotorbikeDTO/ModelResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Models
+{
+    public partial class ModelResponseDTO
+    {
+        public int ModelId { get; set; }
+        public string? ModelName { get; set; }
+        public string? Description { get; set; }
+        public string Status { get; set; } = null!;
+        public virtual BrandRegisterDTO? Brand { get; set; }
+    }
+}
