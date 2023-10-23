f507609789e7143b2f05b91296d96a6f5a79fb25
AvilXx <magician0145632@gmail.com>
2023-10-23 23:54:29 +0700

    Bill: Bill for InStock-Motor

--- API/Controllers/BillController.cs
+++ API/Controllers/BillController.cs
@@ -133,5 +133,108 @@ namespace API.Controllers
                 return BadRequest(_response);
             }
         }
+
+        [HttpPut]
+        [Authorize(Roles = "Store")]
+        [Route("BillforInStock")]
+        public async Task<IActionResult> BillforInStock(int MotorID, int newUser)
+        {
+            try
+            {
+                var obj = await _unitOfWork.MotorBikeService.GetFirst(e => e.MotorId == MotorID);
+                if (obj == null)
+                {
+                    _response.ErrorMessages.Add("Không tìm thấy xe này!");
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    return NotFound(_response);
+                }
+                else
+                {
+                    var userId = int.Parse(User.FindFirst("UserId")?.Value);
+                    if (userId != obj.StoreId && userId != obj.OwnerId)
+                    {
+                        _response.StatusCode = HttpStatusCode.BadRequest;
+                        _response.IsSuccess = false;
+                        _response.ErrorMessages.Add("Xe không thuộc quyền của người dùng!");
+                        return BadRequest(_response);
+                    }
+                    else
+                    {
+                        int check = 0;
+                        foreach (var PostingType in SD.RequestPostingTypeArray)
+                        {
+                            var request = await _unitOfWork.RequestService.GetFirst(
+                                    e => e.MotorId == MotorID && e.RequestTypeId == PostingType && e.Status == SD.Request_Accept
+                            );
+                            if (request != null) { check += 1; }
+                        }
+                        if (check == 0)
+                        {
+                            _response.StatusCode = HttpStatusCode.BadRequest;
+                            _response.IsSuccess = false;
+                            _response.ErrorMessages.Add("Xe chưa được đăng lên sàn!");
+                            return BadRequest(_response);
+                        }
+                    }
+                    //Add Request
+                    Request requestNew = new()
+                    {
+                        MotorId = MotorID,
+                        ReceiverId = newUser,
+                        SenderId = userId,
+                        Time = DateTime.Now,
+                        RequestTypeId = SD.Request_MotorTranfer_Id,
+                        Status = SD.Request_Pending
+                    };
+                    await _unitOfWork.RequestService.Add(requestNew);
+                    //-----------
+                    //Add Cus Bill
+                    var requestCus = await _unitOfWork.RequestService.GetLast(
+                                    e => e.MotorId == MotorID && e.RequestTypeId == SD.Request_MotorTranfer_Id && e.Status == SD.Request_Pending
+                    );
+                    if (requestCus == null)
+                    {
+                        _response.StatusCode = HttpStatusCode.BadRequest;
+                        _response.IsSuccess = false;
+                        _response.ErrorMessages.Add("Không tìm thấy yêu cầu!");
+                        return BadRequest(_response);
+                    }
+                    BillConfirm CusBill = new()
+                    {
+                        MotorId = MotorID,
+                        UserId = userId,
+                        StoreId = (int)obj.StoreId,
+                        Price = obj.Price,
+                        CreateAt = DateTime.Now,
+                        Status = SD.Request_Accept,
+                        RequestId = requestCus.RequestId
+                    };
+                    await _unitOfWork.BillService.Add(CusBill);
+                    //Update request to done
+                    requestCus.Status = SD.Request_Accept;
+                    await _unitOfWork.RequestService.Update(requestCus);
+                    //Update Motor Ownership
+                    obj.MotorStatusId = SD.Status_Storage;
+                    obj.OwnerId = newUser;
+                    await _unitOfWork.MotorBikeService.Update(obj);
+                    //---------------------
+                    _response.IsSuccess = true;
+                    _response.StatusCode = HttpStatusCode.OK;
+                    _response.Result = CusBill;
+                    return Ok(_response);
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.ErrorMessages = new List<string>()
+                {
+                    ex.ToString()
+                };
+                return BadRequest(_response);
+            }
+        }
     }
 }
