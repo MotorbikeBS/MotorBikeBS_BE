d7eee7c7bccea11ef31ab12186a9ac8b1150a103
NTPhatFPT <phatntse150133@fpt.edu.vn>
2023-11-11 17:10:05 +0700

    Payment Add-Point History

---
+++ API/DTO/PaymentDTO/PaymentRequestResponseDTO.cs
@@ -0,0 +1,17 @@
+﻿using Core.Models;
+
+namespace API.DTO.PaymentDTO
+{
+    public class PaymentRequestResponseDTO
+    {
+        public int RequestId { get; set; }
+        public int? MotorId { get; set; }
+        public int? ReceiverId { get; set; }
+        public int? SenderId { get; set; }
+        public DateTime? Time { get; set; }
+        public int? RequestTypeId { get; set; }
+        public string? Status { get; set; }
+
+        public virtual ICollection<PaymentResponseDTO> Payments { get; set; }
+    }
+}
